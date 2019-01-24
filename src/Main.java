22695431b29c3acda1357c964f6cd817c6f55368
manos <manos8991@gmail.com>
2019-01-25 01:08:59 +0200

    7commit

--- src/Main.java
+++ src/Main.java
@@ -44,7 +44,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        printMenu(0);//hey
+        printMenu(0);
         String choise;
         Scanner sc = new Scanner(System.in);
         choise = sc.next();
